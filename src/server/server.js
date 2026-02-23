08a3cef70f3e39483734097c6c5653230fe8cd26
shay.groups <shay.groups@gmail.com>
2026-02-23 12:24:27 +0200

    a

--- src/server/server.js
+++ src/server/server.js
@@ -18,8 +18,8 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // increase request body limits
-app.use(express.json({ limit: "50mb" }));
-app.use(express.urlencoded({ extended: true, limit: "50mb" }));
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "../client"), {
